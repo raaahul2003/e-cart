2d19be26e25b02e4c702ab3d291e194007ab2673
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-09-19 10:55:02 +0530

    landing page completed working on wishlist

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Card from 'react-bootstrap/Card';
-import logo from '../assets/logo.png'
-import { Link } from 'react-router';
+import { Link, useParams } from 'react-router';
 import { fetchProducts } from '../redux/slice/productSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import Spinner from 'react-bootstrap/Spinner';
+import Pageination from '../components/Pageination';
 
 function Landing() {
   const dispatch = useDispatch()
   const { loading, allproducts, error } = useSelector(state => state.product)
   console.log(loading, allproducts, error);
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const cardPerPage = 6
+  let endingIndex = currentPage * cardPerPage
+  let startingIndex = endingIndex - cardPerPage
+  if (startingIndex > allproducts?.length) {
+    setCurrentPage(1)
+  }
+  let currentProducts = allproducts.slice(startingIndex, endingIndex)
 
 
   useEffect(() => {
@@ -18,10 +27,6 @@ function Landing() {
     dispatch(fetchProducts())
 
   }, [])
-
-
-
-
 
   return (
     <>
@@ -34,18 +39,18 @@ function Landing() {
             </Spinner>
           </div>
           :
-          <div className='container my-5'>
+          <div className='container my-3'>
             <div className='row'>
               {
-                allproducts?.length > 0 ?
-                  allproducts?.map(pro => (
-                    <div className='col-lg-4 col-md-4 col-md-6  mt-3'>
+                currentProducts?.length > 0 ?
+                  currentProducts?.map(pro => (
+                    <div className='col-lg-4 col-md-6  mt-3'>
                       <Card className='p-2 shadow rounded' style={{ width: '18rem' }}>
                         <Card.Img variant="top" src={pro.thumbnail} />
                         <Card.Body>
                           <Card.Title className='text-center'>{pro.title.slice(0, 15)}</Card.Title>
                           <div className='text-center'>
-                            <Link to={'/product/1/view'} className='fw-bold text-decoration-none'>View More</Link>
+                            <Link to={`/product/${pro.id}/view`} className='fw-bold text-decoration-none'>View More</Link>
                           </div>
                         </Card.Body>
                       </Card>
@@ -54,6 +59,9 @@ function Landing() {
                   :
                   <p className='text-center fw-bold'>No Produts Found!</p>
               }
+            </div>
+            <div className='text-center mt-3'>
+              <Pageination totalProducts={allproducts?.length} productPerpage={cardPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
             </div>
           </div>
       }
--- src/redux/slice/productStore.js
+++ src/redux/slice/productStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from '../slice/productSlice'
+import wishListReducer from '../slice/wishlistSlice'
+
 
 const store = configureStore({
     reducer: {
-        product: productReducer
+        product: productReducer,
+        wishlist: wishListReducer
 
     }
 })
