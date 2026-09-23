abfc8a79776092eb2406a86310d766cb7f9da890
Rahul Raj C P <rahulrajcp@Rahuls-MacBook-Air.local>
2026-09-23 10:06:34 +0530

    modified the cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
 import { useDispatch, useSelector } from 'react-redux'
-import { removeFromCart } from '../redux/slice/cartSlice'
+import { decrementQuantity, emptyCart, incrementQuantity, removeFromCart } from '../redux/slice/cartSlice'
+import { Link, Navigate, useNavigate } from 'react-router'
+
+
 
 function Cart() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   const cart = useSelector((state) => state.cart)
+
+  const checkout = () => {
+    alert("order placed successfully!")
+    dispatch(emptyCart())
+    navigate("/")
+
+  }
 
   return (
     <>
@@ -35,37 +46,45 @@ function Cart() {
                         <td><img src={pro.thumbnail} alt={pro.title} width={'40px'} /></td>
                         <td>
                           <div className='d-flex align-items-center'>
-                            <button className='btn'>-</button>
+                            {
+                              pro?.quantity > 1 &&
+                              <button className='btn' onClick={() => dispatch(decrementQuantity(pro?.id))}>-</button>
+                            }
                             <input
                               type='text'
-                              style={{ width: '20px', border: 'none' }}
+                              style={{ width: '30px', border: 'none' }}
                               className='border-none'
                               value={pro?.quantity}
                               readOnly
                             />
-                            <button className='btn'>+</button>
+                            <button onClick={() => dispatch(incrementQuantity(pro?.id))} className='btn'>+</button>
                           </div>
                         </td>
-                        <td>{pro.price}</td>
+                        <td>{pro.totalPrice}</td>
                         <td>
                           <button onClick={() => dispatch(removeFromCart(pro?.id))} className='btn'><i className='fa-solid fa-trash text-danger fa-2x1'></i></button>
                         </td>
                       </tr>
                     ))}
                   </tbody>
+                  <div className='mt-5 d-flex gap-2'>
+                    <Link to={'/'} className='btn btn-info rounded'>Shop More</Link>
+                    <button onClick={() => dispatch(emptyCart())} className='btn btn-danger rounded'>Cart Reset</button>
+                  </div>
                 </table>
 
-                <div className='mt-5'>
-                  <button className='btn btn-info rounded'>Shop More</button>
-                  <button className='btn btn-info rounded'>Cart Reset</button>
-                </div>
+
               </div>
 
               <div className='col-lg-4'>
                 <div className='shadow rounded p-5'>
 
-                  <h3 className='text-warning'>Total Products: <span className='text-danger'>0</span></h3>
-                  <h3 className='text-warning'>Total Price: <span className='text-danger'>0</span></h3>
+                  <h3 className='text-warning'>Total Products:<span className='text-danger'> {cart?.length}</span></h3>
+                  <h3 className='text-warning'>Total Price: <span className='text-danger'>{
+                    cart?.reduce((a, b) => 
+                      a + b.totalPrice,0
+                    )}</span></h3>
+                  <button className='btn btn-success rounded' onClick={checkout}>Check Out</button>
                 </div>
 
               </div>
